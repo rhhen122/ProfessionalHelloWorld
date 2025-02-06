43886ea0173d3ff25da41653f3a4e4b5c12ce99c
RHHEN122 <rokyhenderson222@gmail.com>
2025-02-06 19:05:19 +1300

    v1.8

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-def importvars():
-    from assets.comma import comma
-    from assets.space import space
-    from assets.h import H
-    from assets.e import e
-    from assets.l import l
-    from assets.o import o
-    from assets.w import W
-    from assets.r import r
-    from assets.d import d
-importvars()
+from assets.comma import comma
+from assets.space import space
+from assets.h import H
+from assets.e import e
+from assets.l import l
+from assets.o import o
+from assets.w import W
+from assets.r import r
+from assets.d import d
 print(H+e+l+l+o+comma+space+W+o+r+l+d)
