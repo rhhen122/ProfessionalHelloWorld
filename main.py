b0d2576f1e6b36839fcecde02ef5bc02dfac57b9
RHHEN122 <rokyhenderson222@gmail.com>
2025-02-06 18:52:44 +1300

    v1.4

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def importvars():
     from assets.bang import bang
     from assets.comma import comma
-    from assets.h import h
+    from assets.H import H
     from assets.e import e
     from assets.l import l
     from assets.o import o
-    from assets.w import w
+    from assets.W import W
     from assets.r import r
     from assets.d import d
 importvars()
-print(h+e+l+l+o+comma+space+w+o+r+l+d) #type: ignore
+print(H+e+l+l+o+comma+space+W+o+r+l+d) #type: ignore
