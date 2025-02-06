b9feae26289f8712bed045c95c0a78cd84c58683
RHHEN122 <rokyhenderson222@gmail.com>
2025-02-06 19:00:13 +1300

    v1.5

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def importvars():
     from assets.bang import bang
     from assets.comma import comma
-    from assets.H import H
+    from assets.h import H
     from assets.e import e
     from assets.l import l
     from assets.o import o
-    from assets.W import W
+    from assets.w import W
     from assets.r import r
     from assets.d import d
 importvars()
