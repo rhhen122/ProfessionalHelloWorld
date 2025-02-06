ea8054a1e6a3d2e32e79472919c5bc496cc92056
RHHEN122 <rokyhenderson222@gmail.com>
2025-02-06 18:16:08 +1300

    v1.0

---
+++ main.py
@@ -0,0 +1,12 @@
+def importvars():
+    from assets.bang import bang
+    from assets.comma import comma
+    from assets.h import h
+    from assets.e import e
+    from assets.l import l
+    from assets.o import o
+    from assets.w import w
+    from assets.r import r
+    from assets.d import d
+importvars()
+print(h+e+l+l+o+comma+space+w+o+r+l+d) #type: ignore
